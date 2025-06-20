0bbfb7090e6c1c90887ab34c5f42d3c43c7abb06
piramura <kesunouko2017@gmail.com>
2025-06-20 17:03:09 +0900

    add api key

--- index.js
+++ index.js
@@ -6,7 +6,21 @@ const port = process.env.PORT || 3000;
 // ミドルウェア
 app.use(cors()); // GitHub Pages など外部からのアクセスを許可
 app.use(express.json());
+// ミドルウェアとして導入（index.jsなど）
+app.use((req, res, next) => {
+  const key = req.headers['x-api-key'];
+  const allowedRoutes = ['GET']; // 誰でもアクセスOKなメソッド
 
+  if (allowedRoutes.includes(req.method)) {
+    return next();
+  }
+
+  if (key !== process.env.API_KEY) {
+    return res.status(403).json({ error: 'Forbidden' });
+  }
+
+  next();
+});
 // 各ルートの読み込み
 const blogRoutes = require('./routes/blogs');
 const gameRoutes = require('./routes/games');
